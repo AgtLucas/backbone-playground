500a3a2aa31aececb30b226e245b43ddbffb654b
Lucas <metallucas.ds@gmail.com>
2013-06-17 23:22:56 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,3 +10,40 @@ var Todo = Backbone.Model.extend({
 var myTodo = new Todo({
     title: 'Check attributes property of the logged models in the console.'
 });
+
+var TodoView = Backbone.View.extend({
+    tagName: 'li',
+
+    todoTpl: _.template( $('#item-template').html() ),
+
+    events: {
+        'dblclick label': 'edit',
+        'keypress .edit': 'updateOnEnter',
+        'blur .edit': 'close'
+    },
+
+    initialize: function() {
+        this.$el = $('#todo');
+    },
+
+    render: function() {
+        this.$el.html( this.todoTpl( this.model.JSON() ) );
+
+        this.input = this.$('.edit');
+        return this;
+    },
+
+    edit: function() {
+
+    },
+
+    close: function() {
+
+    },
+
+    updateOnEnter: function( e ) {
+
+    }
+});
+
+var todoView = new TodoView({model: myTodo});
