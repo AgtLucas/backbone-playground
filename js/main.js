02d7f273862d3d2650440dadb4c428cb39cc6c12
Lucas <metallucas.ds@gmail.com>
2013-06-17 23:15:16 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+// Define a Todo Model
+var Todo = Backbone.Model.extend({
+    // Default todo attribute values
+    defaults: {
+        title: '',
+        completed? false
+    }
+});
 
+var myTodo = new Todo({
+    title: 'Check attributes property of the logged models in the console.'
+});
